c92582aa4cc8f149a9a2ca3cda00bace96e5a8b3
Greg Wang <greg.wang@genesys.com>
2015-04-06 18:07:55 -0400

    First implementation.

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 'use strict';
-module.exports = function (str) {
-  console.log(str || 'Rainbow');
+var bodyParser = require('body-parser');
+var csv = require('csv');
+
+module.exports = function (options, parseOptions) {
+  var textOptions = Object.create({}, options);
+  textOptions.type = 'text/csv';
+  var textMiddleware = bodyParser.text(textOptions);
+
+  return function (req, res, next) {
+    if (req.get('content-type') !== 'text/csv') {
+      next();
+    }
+
+    textMiddleware(req, res, function (err) {
+      if (err || Object.prototype.toString.call(req.body) !== '[object String]') {
+        return next(err);
+      }
+
+      csv.parse(req.body, parseOptions || {}, function(err, parsed) {
+        if (err) {
+          return next(err);
+        }
+        req.body = parsed;
+        next();
+      });
+    });
+  };
 };
